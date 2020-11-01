cd38a54166e3631145f4100a0b662d439e29cbf1
ghiffarikemal <58480251+ghiffarikemal@users.noreply.github.com>
2020-11-02 05:11:37 +0700

    Delete no1.js

--- no1.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str) {
-    var currentString = str;
-    var newString = '';for (let i = str.length - 1; i >= 0; i--) {
-     newString = newString + currentString[i];
-    }
-    
-    return newString;
-   }console.log(reverseString('hello'));
